ebe0099ca8bdbf7a4395816ee8c994ce02eb045c
rudy <rboinnard@alpha3i.com>
2023-02-05 12:57:08 +0100

    Setup : Elegant file structure

--- dev/utils/scripts/monitor.js
+++ dev/utils/scripts/monitor.js
@@ -4,27 +4,25 @@ const fs = require('fs');
 
 // Var used to trigger initialization log messages. It is required by vsCode tasks (attribute : "ProblemMatcher").
 // The ProblemMatcher is waiting for start and end messages before launching the debugger
-var init = false;
+var problemMatcherStartMessage = "Files change detected";
+var problemMatcherEndMessage = "Files processed : watching changes...";
 
-// One-liner for current directory
-chokidar.watch('./syntaxes/**/*.yaml').on('all', (event, path) => {
-    if (!init) {
-        console.log("Initializing watched directories");
-    }
+// tmLanguage watcher
+chokidar.watch('./src/data/syntaxes/wlanguage.tmLanguage.yaml').on('all', (event, path) => {
+    console.log(problemMatcherStartMessage);
 
     if (['add', 'change'].includes(event)) {
         console.log(event, path);
         // Get document, or throw exception on error
         try {
             const jsonData = JSON.stringify(yaml.load(fs.readFileSync(path, 'utf8')), null, '    ');
-            if (jsonData == undefined) throw `Fle ${path} is empty, file generation skipped`;
-            fs.writeFile(`${path.replace(/\.[^/.]+$/, "")}.json`, jsonData, (err) => {if (err) throw err});
+            if (jsonData == undefined) throw `File ${path} is empty, file generation skipped`;
+            let destination = `${path.replace('src', 'dist').replace(/\.[^/.]+$/, "")}.json`;
+            fs.writeFile(destination, jsonData, (err) => {if (err) throw err});
         } catch (e) {
             console.log(e);
         }
     }
 
-    if (!init) {
-        console.log("Initialization completed : watching changes...");
-    }
+    console.log(problemMatcherEndMessage);
 });
