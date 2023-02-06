20b64267d4d253a3d88c84e265c6ffffe272ed3e
Tom Chappaz <tchappaz@alpha3i.com>
2023-02-06 09:55:11 +0100

    monitor: create dist if necessary

--- language/src/scripts/monitor.js
+++ language/src/scripts/monitor.js
@@ -1,6 +1,7 @@
 const chokidar = require('chokidar');
 const yaml = require('js-yaml');
 const fs = require('fs');
+const path = require('path');
 
 // Var used to trigger initialization log messages. It is required by vsCode tasks (attribute : "ProblemMatcher").
 // The ProblemMatcher is waiting for start and end messages before launching the debugger
@@ -8,17 +9,21 @@ var problemMatcherStartMessage = "Files change detected";
 var problemMatcherEndMessage = "Files processed : watching changes...";
 
 // tmLanguage watcher
-chokidar.watch('./language/src/data/syntaxes/wlanguage.tmLanguage.yaml').on('all', (event, path) => {
+chokidar.watch('./language/src/data/syntaxes/wlanguage.tmLanguage.yaml').on('all', (event, pathString) => {
     console.log(problemMatcherStartMessage);
 
     if (['add', 'change'].includes(event)) {
-        console.log(event, path);
+        console.log(event, pathString);
         // Get document, or throw exception on error
         try {
-            const jsonData = JSON.stringify(yaml.load(fs.readFileSync(path, 'utf8')), null, '    ');
-            if (jsonData == undefined) throw `File ${path} is empty, file generation skipped`;
-            let destination = `${path.replace('src', 'dist').replace(/\.[^/.]+$/, "")}.json`;
-            fs.writeFile(destination, jsonData, (err) => {if (err) throw err});
+            const jsonData = JSON.stringify(yaml.load(fs.readFileSync(pathString, 'utf8')), null, '    ');
+            if (jsonData == undefined) throw `File ${pathString} is empty, file generation skipped`;
+            let destination = `${pathString.replace('src', 'dist').replace(/\.[^/.]+$/, "")}.json`;
+
+            fs.mkdir(path.dirname(destination), { recursive: true }, (err) => {
+                if (err) throw err;
+                fs.writeFile(destination, jsonData, (err) => { if (err) throw err });
+            });
         } catch (e) {
             console.log(e);
         }
